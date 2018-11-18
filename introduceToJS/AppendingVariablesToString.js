78eb24866f8d78c568c91749bf7542ab549c8687
Ann <anna.muzykina83@gmail.com>
2018-11-18 22:42:00 +0200

    Add files via upload

---
+++ introduceToJS/AppendingVariablesToString.js
@@ -0,0 +1,14 @@
+/**Just as we can build a string over multiple lines out of string literals, we can also append variables to a string using the plus equals (+=) operator.
+
+
+Set someAdjective and append it to myStr using the += operator. */
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "very amasing idea!";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
---
+++ introduceToJS/ConstructingStringsWithVariables.js
@@ -0,0 +1,12 @@
+/*Sometimes you will need to build a string, Mad Libs style. By using the concatenation operator (+), you can insert one or more variables into a string you're building.
+
+
+Set myName to a string equal to your name and build myStr with myName between the strings "My name is " and " and I am well!"
+ */
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Anna";
+var myStr = "My name is " + myName + " and I am well!";
