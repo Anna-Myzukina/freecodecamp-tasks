63f8df39e857b37e6b881bb275463c9ea6e8ad76
Ann <anna.muzykina83@gmail.com>
2018-11-19 09:03:41 +0200

    Add files via upload

---
+++ introduceToJS/ArrayWithinAnotherArray.js
@@ -0,0 +1,10 @@
+/**Nest one Array within Another Array
+You can also nest arrays within other arrays, like this: [["Bulls", 23], ["White Sox", 45]]. This is also called a Multi-dimensional Array.
+
+
+Create a nested array called myArray. */
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [["Anna", 35], ["Hello World"], 8888];
